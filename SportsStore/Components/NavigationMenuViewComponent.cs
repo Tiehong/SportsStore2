9c319d708f5e975eb9edb9a31f519e2b0374a4c6
Tiehong Xiao <tiehong@chiwater.com>
2017-12-03 15:24:49 -0500

    Add navigation menu at left

---
+++ SportsStore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using SportsStore.Models;
+
+namespace SportsStore.Components {
+
+    public class NavigationMenuViewComponent : ViewComponent {
+        private IProductRepository repository;
+
+        public NavigationMenuViewComponent(IProductRepository repo) {
+            repository = repo;
+        }
+
+        public IViewComponentResult Invokes() {
+            ViewBag.SelectedCategory = RouteData?.Values["category"];
+            return View(repository.Products
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x));
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(repository.Products
+                 .Select(x => x.Category)
+                 .Distinct()
+                 .OrderBy(x => x));
+        }
+    }
+}
