fa3058b38c3d9d7a8fc454559bfd075b616d4486
Tiehong Xiao <tiehong@chiwater.com>
2017-12-03 18:09:28 -0500

    Complete shopping cart

---
+++ SportsStore/createOrder.sql
@@ -0,0 +1,25 @@
+﻿use SportsStore
+drop table Orders
+CREATE TABLE [dbo].[Orders] (
+    [OrderID]     INT             IDENTITY (1, 1) NOT NULL,
+    [Name]          NVARCHAR (100)  NOT NULL,
+    [Line1]	    NVARCHAR (200)  NOT NULL,
+    [Line2]	    NVARCHAR (200)  NULL,
+    [Line3]	    NVARCHAR (200)  NULL,
+    [City]          NVARCHAR (100)   NOT NULL,
+    [State]         NVARCHAR (50)   NOT NULL,
+    [Zip]           NVARCHAR (10)   NOT NULL,
+    [Country]        NVARCHAR (50)   NOT NULL,
+    [Giftwrap]      INT,
+    PRIMARY KEY CLUSTERED ([OrderID] ASC)
+);
+
+Create Table CartLine(
+	[CartLineID]     INT             IDENTITY (1, 1) NOT NULL,
+	[OrderId] INT NOT NULL,
+	[ProductId] INT NOT NULL,
+	[Quantity] INT NOT NULL,
+    PRIMARY KEY CLUSTERED ([CartLineID] ASC),
+    FOREIGN KEY (ProductID) REFERENCES Products(ProductID),
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+);
---
+++ SportsStore/createTable.sql
@@ -0,0 +1,12 @@
+﻿use SportsStore
+drop table Products
+CREATE TABLE [dbo].[Products] (
+    [ProductID]     INT             IDENTITY (1, 1) NOT NULL,
+    [Name]          NVARCHAR (100)  NOT NULL,
+    [Description]   NVARCHAR (500)  NOT NULL,
+    [Category]      NVARCHAR (50)   NOT NULL,
+    [Price]         DECIMAL (16, 2) NOT NULL,
+    [ImageMimeType] VARCHAR (50)    NULL,
+    [ImageData]     VARBINARY (MAX) NULL,
+    PRIMARY KEY CLUSTERED ([ProductID] ASC)
+);
