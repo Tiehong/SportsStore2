ed70f382f68a16820136fb13805c3100285a1d94
Tiehong Xiao <tiehong@chiwater.com>
2017-12-07 15:37:59 -0500

    Update fake product repo

--- SportsStore/Models/FakeProductRepository.cs
+++ SportsStore/Models/FakeProductRepository.cs
@@ -6,22 +6,35 @@ namespace SportsStore.Models
 
     public class FakeProductRepository : IProductRepository
     {
+        List<Product> products;
 
-        public IQueryable<Product> Products => new List<Product> {
-            new Product { Name = "Football", Price = 25 },
-            new Product{Name="Shin Pad",Price=18},
-            new Product { Name = "Surf board", Price = 179 },
-            new Product { Name = "Running shoes", Price = 95 }
-        }.AsQueryable<Product>();
+        public IQueryable<Product> Products
+        {
+            get
+            {
+                if (products == null)
+                    products = new List<Product> {
+                    new Product { ProductID = 1, Category = "Soccer", Name = "Football", Price = 25 },
+                    new Product { ProductID = 2, Category = "Soccer", Name = "Shin Pad", Price=18},
+                    new Product { ProductID = 3, Category = "WaterSports", Name = "Surf board", Price = 179 },
+                    new Product { ProductID = 4, Category = "Field", Name = "Running shoes", Price = 95 }
+                };
+                return products.AsQueryable<Product>();
+            }
+        }
 
         public Product DeleteProduct(int productID)
         {
-            throw new System.NotImplementedException();
+            Product p = products.FirstOrDefault(x => x.ProductID == productID);
+            if (p != null)
+                products.Remove(p);
+            return p;
         }
 
         public void SaveProduct(Product product)
         {
-            throw new System.NotImplementedException();
+            product.ProductID = Products.Count() + 1;
+            products?.Add(product);
         }
     }
 }
